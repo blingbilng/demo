3fe500fd81d61fb1c06f8e47490981a7ca92b24d
zhang.liangliang <zhangliangliang@ouyada.com>
2020-11-24 15:38:56 +0800

    init

---
+++ src/test/java/com/lmfun/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lmfun.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
